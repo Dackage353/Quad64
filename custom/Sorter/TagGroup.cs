c21e2e51fdeb022cfdd21c926313915c84656785
Daniel <daniel.g.hackel@gmail.com>
2026-05-27 23:05:02 -0500

    rename tagsorter, adapt for use by main sorter

---
+++ custom/Sorter/TagGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quad64.custom.Sorter
+{
+    internal class TagGroup
+    {
+        public List<Object3D> Items { get; } = new List<Object3D>();
+        public string Tag { get; }
+
+        public TagGroup(string tag)
+        {
+            Tag = tag;
+        }
+
+    }
+}
---
+++ custom/Sorter/TagSorter.cs
@@ -0,0 +1,93 @@
+using NaturalSort.Extension;
+using OpenTK;
+using Quad64.custom.Sorter;
+using Quad64.src.LevelInfo;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Windows.Forms;
+
+namespace Quad64.src
+{
+    internal class TagSorter
+    {
+        private readonly string[] TagOrder = new string[]
+            {
+                "WarpStart",
+                "WarpRegular",
+                "Star",
+                "StarTrigger",
+                "CapOrShellBox",
+                "Container",
+                "Switch",
+                "BlueCoin",
+                "HiddenBox",
+                "CoinContainer",
+                "CoinFormation",
+                "Coin",
+                "Enemy",
+                "Obstacle",
+                "Platform",
+                "Environment",
+                "Dialog",
+                "WarpExit"
+            };
+
+
+        public List<Object3D> Sort(List<Object3D> list)
+        {
+            if (list.Count <= 1) return list;
+
+            var groups = SortIntoGroups(list);
+
+            var finalList = new List<Object3D>();
+            foreach (var group in groups)
+            {
+                finalList.AddRange(group.Items);
+            }
+
+            return finalList;
+        }
+
+        public List<TagGroup> SortIntoGroups(List<Object3D> list)
+        {
+            List<TagGroup> groups = new List<TagGroup>();
+            var remaining = new List<Object3D>(list);
+
+            for (int i = 0; i < TagOrder.Length; i++)
+            {
+                var group = new TagGroup(TagOrder[i]);
+                var type = TagOrder[i];
+
+                foreach (var obj in remaining)
+                {
+                    if (obj.CompanionInfo != null && obj.CompanionInfo.Types.Contains(type))
+                    {
+                        group.Items.Add(obj);
+                    }
+                }
+
+                foreach (var obj in group.Items)
+                {
+                    remaining.Remove(obj);
+                }
+
+                groups.Add(group);
+            }
+
+            var uncategorized = new TagGroup("Uncategorized");
+            foreach (var info in remaining)
+            {
+                if (info.getObjectComboName() != "ZZ Empty Object")
+                {
+                    uncategorized.Items.Add(info);
+                }
+            }
+            groups.Add(uncategorized);
+
+            return groups;
+        }
+    }
+}
