609c6acab74900d9f071650224cc5bd385f13ab9
Daniel <daniel.g.hackel@gmail.com>
2026-05-27 23:05:10 -0500

    new main sorter

---
+++ custom/Sorter/Sorter.cs
@@ -0,0 +1,39 @@
+﻿using Quad64.src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quad64.custom.Sorter
+{
+    internal class Sorter
+    {
+        public List<Object3D> SortByTypeThenNameThenDistance(List<Object3D> list)
+        {
+            if (list.Count <= 1) return list;
+
+            var finalSorted = new List<Object3D>();
+
+            var typeGroups = new TagSorter().SortIntoGroups(list);
+            foreach (var typeGroup in typeGroups)
+            {
+                if (typeGroup.Tag == "Star")
+                {
+                    finalSorted.AddRange(new StarSorter().Sort(typeGroup.Items));
+                }
+                else
+                {
+                    var nameGroups = new NameSorter().SortIntoGroups(typeGroup.Items);
+                    foreach (var nameGroup in nameGroups)
+                    {
+                        var distanceList = new DistanceSorter().SortByDistance(nameGroup);
+                        finalSorted.AddRange(distanceList);
+                    }
+                }
+            }
+
+            return finalSorted;
+        }
+    }
+}
