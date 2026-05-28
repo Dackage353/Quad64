a2d650510995c267239ec38ece86fe81713e902e
Daniel <daniel.g.hackel@gmail.com>
2026-05-27 23:06:53 -0500

    in helper.cs, re-ordered cases

--- custom/Helper.cs
+++ custom/Helper.cs
@@ -21,22 +21,22 @@ public static class Helper
             case 0x0D: return "Course 13";
             case 0x0E: return "Course 14";
             case 0x0F: return "Course 15";
-            case 0x10: return "Overworld 1";
-            case 0x06: return "Overworld 2";
-            case 0x1A: return "Overworld 3";
+            case 0x1B: return "Secret Level 1";
+            case 0x14: return "Secret Level 2";
+            case 0x1F: return "Secret Level 3";
+            case 0x19: return "Secret Level 4";
+            case 0x1C: return "Cap Level, Metal Cap";
+            case 0x1D: return "Cap Level, Wing Cap";
+            case 0x12: return "Cap Level, Vanish Cap";
             case 0x11: return "Bowser 1 course";
             case 0x13: return "Bowser 2 course";
             case 0x15: return "Bowser 3 course";
             case 0x1E: return "Bowser 1 fight";
             case 0x21: return "Bowser 2 fight";
             case 0x22: return "Bowser 3 fight";
-            case 0x1C: return "Cap Level, Metal Cap";
-            case 0x1D: return "Cap Level, Wing Cap";
-            case 0x12: return "Cap Level, Vanish Cap";
-            case 0x1B: return "Secret Level 1";
-            case 0x14: return "Secret Level 2";
-            case 0x1F: return "Secret Level 3";
-            case 0x19: return "Secret Level 4";
+            case 0x10: return "Overworld 1";
+            case 0x06: return "Overworld 2";
+            case 0x1A: return "Overworld 3";
         }
 
         return string.Empty;
