d87d7e381864803db88339eaaf4b31b3bd3158e6
Daniel <daniel.g.hackel@gmail.com>
2026-05-27 23:05:39 -0500

    rename and rework objectinfo

---
+++ custom/GameInfo/ObjectInfo.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quad64.custom.GameInfo
+{
+    internal class ObjectInfo
+    {
+        public string Name { get; set; }
+        public string[] Types { get; set; }
+        public int CoinValue { get; set; } = 0;
+        public int StarNumber { get; set; } = -1;
+        public string BehaviorAddress { get; set; }
+        public int ModelID { get; set; } = -1;
+        public int Param1 { get; set; } = -1;
+        public int Param2 { get; set; } = -1;
+        public int Param3 { get; set; } = -1;
+        public int Param4 { get; set; } = -1;
+    }
+}
